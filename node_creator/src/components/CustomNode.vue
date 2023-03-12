6003550467f1a425cca55bebdedb7bf69c50fd06
mirceascarlatescu <scarlatescu.mircea8@gmail.com>
2023-03-12 01:29:24 +0000

    Added custom node + I/O

---
+++ node_creator/src/components/CustomNode.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Handle, Position } from '@vue-flow/core'
+import { NodeToolbar } from '@vue-flow/node-toolbar'
+
+defineProps(['data', 'label'])
+</script>
+
+<template>
+    <div>
+        <NodeToolbar
+            style="display: flex; gap: 0.5rem; align-items: center"
+            :is-visible="data.toolbarVisible"
+            :position="data.toolbarPosition"
+        >
+            <button>Add input</button>
+            <button>Add Output</button>
+        </NodeToolbar>
+
+        <div :style="{ padding: '10px 20px' }">
+            {{ label }}
+
+        </div>
+        <div v-for="[key, value] in data.inputs" :style="{position: Position.Left}">
+            <li>
+                <Handle type="target" :position="Position.Left" /> {{ key }} : {{ value }}
+            </li>
+        </div>
+        <div v-for="[key, value] in data.outputs" :style="{position: Position.Right}">
+            <li>
+                {{ key }} : {{ value }} <Handle type="target" :position="Position.Right" /> 
+            </li>
+        </div>
+    </div>
+</template>
--- node_creator/src/components/NodeCreator.vue
+++ node_creator/src/components/NodeCreator.vue
@@ -2,11 +2,13 @@
 
 import { ref } from 'vue';
 import { reactive } from 'vue';
+import { markRaw } from 'vue'
 import type { Elements } from '@vue-flow/core'
-import { VueFlow, Node, useVueFlow } from '@vue-flow/core'
+import { VueFlow, Node, useVueFlow, Position } from '@vue-flow/core'
 import { Background, BackgroundVariant } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
 import { MiniMap } from '@vue-flow/minimap'
+import CustomNode from './CustomNode.vue'
 
 import "@vue-flow/core/dist/style.css";
 import "@vue-flow/core/dist/theme-default.css";
@@ -15,15 +17,15 @@ import "@vue-flow/minimap/dist/style.css";
 
 const { nodes, addNodes, edges, addEdges, onConnect, onPaneReady, onNodeDragStop, dimensions, findNode, setNodes, updateNodeInternals } = useVueFlow()
 
-onConnect((params) => addEdges([params]))
-onPaneReady((flowInstance) => console.log('flow loaded:', flowInstance))
-onNodeDragStop((node) => console.log('drag stop', node))
-
 
 const defaultLabel = '-'
 
 const opts = reactive({
   label: defaultLabel,
+  inputName: "",
+  inputType: "",
+  outputName: "",
+  outputType: ""
 })
 const updateNode = () => {
   const node = findNode(currentNode.NodeId)
@@ -32,14 +34,17 @@ const updateNode = () => {
 }
 
 const updateNodeAddInput = () => {
-   var container = document.getElementById("dynamic_inputs");
-   var input = document.createElement("input");
-   input.type = "text";
-   container.appendChild(input);
+  const node = findNode(currentNode.NodeId)
+  node.data.inputs.set(opts.inputName,opts.inputType);
+  updateNodeInternals([node.id])
+  
 }
 
 const updateNodeAddOutput = () => {
-
+  const node = findNode(currentNode.NodeId)
+  node.data.outputs = new Map<string, string>();
+  node.data.outputs.set(opts.outputName,opts.outputType);
+  updateNodeInternals([node.id])
 }
 
 interface CurrentNode {
@@ -61,12 +66,15 @@ const addRandomNode = () => {
     id: nodeId,
     label: `Node: ${nodeId}`,
     type: 'custom',
+    template: markRaw(CustomNode),
+    data: {toolbarPosition: Position.Bottom, inputs: new Map<string, string>()},
     position: { x: Math.random() * dimensions.value.width, y: Math.random() * dimensions.value.height },
     selectable: true,
     events: {
       click: () => {
         currentNode.NodeId = newNode.id;
         console.log('currentNode', currentNode.NodeId);
+        //newNode.data.toolbarVisible = true;
         opts.label = findNode(currentNode.NodeId).label;
       },
 
@@ -74,6 +82,15 @@ const addRandomNode = () => {
   };
   addNodes([newNode])
 }
+
+onConnect((params) => addEdges([params]))
+onPaneReady((flowInstance) => console.log('flow loaded:', flowInstance))
+onNodeDragStop((node) => {
+  currentNode.NodeId = node.node.id;
+  opts.label = node.node.label;
+})
+
+
 </script>
 <template>
   <VueFlow>
@@ -88,9 +105,17 @@ const addRandomNode = () => {
       <label>label:</label>
       <input v-model="opts.label" @input="updateNode"/>
       <div id="dynamic_inputs">
-        <button type="button" @click="updateNodeAddInput">Add Input</button>
+      <label>Name:</label>
+      <input v-model="opts.inputName" @input="updateNode" id="inputName"/>
+      <label>Type:</label>
+      <input v-model="opts.inputType" @input="updateNode" id="inputType"/>
+      <button type="button" @click="updateNodeAddInput">Add Input</button>
       </div>
       <div id="dynamic_outputs">
+        <label>Name:</label>
+        <input v-model="opts.outputName" @input="updateNode" id="inputName"/>
+        <label>Type:</label>
+        <input v-model="opts.outputType" @input="updateNode" id="inputType"/>
         <button type="button" @click="updateNodeAddOutput">Add Output</button>
       </div>
     </div>
@@ -101,4 +126,10 @@ const addRandomNode = () => {
 .updatenode__controls{position:absolute;right:10px;top:10px;z-index:4;font-size:11px;background-color:#d3d3d3;border-radius:10px;padding:8px}
 .updatenode__controls label{display:blocK}
 .updatenode__controls input{padding:2px;border-radius:5px}
+
+.vue-flow__node-custom {
+  background: #9CA8B3;
+  color: #fff;
+  padding: 10px;
+}
 </style>
